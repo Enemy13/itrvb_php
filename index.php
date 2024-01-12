eb56d47b1a651387f8a90aa6ff55428ab58eded2
enemy13 <dimabazit@mail.ru>
2024-01-13 02:20:07 +0500

    fix

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . "/vendor/autoload.php";
 use Tgu\Bazitov\User;
 use Tgu\Bazitov\Article;
 use Tgu\Bazitov\Comment;
+use Faker\Factory;
 
 spl_autoload_register(function ($class) {
     $file = str_replace(['\\', '_'], [DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR], $class);
@@ -14,7 +15,9 @@ spl_autoload_register(function ($class) {
     }
 });
 
-$user = new User('enemy13', 'Dmitriy', 'Bazitov');
+$faker = Factory::create('ru_RU');
+
+$user = new User($faker->userName(), $faker->firstName(), $faker->lastName());
 
 echo $user->getUserName();
 echo $user->getId();
